edfcd9fb51227199a2555904bba4a4a0c185ff5e
zhouyunquan <zhouyunquan@dm-maker.com>
2020-05-26 14:12:23 +0800

    1.1.0 commit

--- DMHomeLinkSDK/Classes/DMLinkFramework.framework/Headers/HttpRequest.h
+++ DMHomeLinkSDK/Classes/DMLinkFramework.framework/Headers/HttpRequest.h
@@ -7,7 +7,6 @@
 //
 
 #import <Foundation/Foundation.h>
-#import "AFNetworking.h"
 
 
 NS_ASSUME_NONNULL_BEGIN
@@ -16,8 +15,6 @@ typedef void(^successBlock)(NSURLSessionDataTask *request,id responseObject);
 typedef void(^errorBlock)(NSURLSessionDataTask *request,NSError *error);
 
 @interface HttpRequest : NSObject
-
-+(AFHTTPSessionManager *)sharedHttpRequest;
 
 //GET请求
 +(void)doGetWithURL:(NSString *)url withParams:(NSDictionary *)params  success:(successBlock)done failure:(errorBlock)fail;
--- DMHomeLinkSDK/Classes/DMLinkFramework.framework/Headers/WiFiBaseManager.h
+++ DMHomeLinkSDK/Classes/DMLinkFramework.framework/Headers/WiFiBaseManager.h
@@ -7,7 +7,7 @@
 //
 
 #import <Foundation/Foundation.h>
-
+#import <UIKit/UIKit.h>
 #import "HttpRequest.h"
 
 #define WIFI_START_ORDER    @"FACEAA00"
